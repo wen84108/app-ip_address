08d07d5e6ab1e6934419b832abb7c9324016e44d
wen84108 <wen.wang1@centurylink.com>
2020-03-18 14:38:23 +0000

    Lab 2 part 1 of 2

--- main.js
+++ main.js
@@ -46,46 +46,6 @@ function getFirstIpAddress(cidrStr, callback) {
 }
 
 
-/*
-  This section is used to test function and log any errors.
-  We will make several positive and negative tests.
-*/
-function main() {
-  // Create some test data for getFirstIpAddress(), both valid and invalid.
-  let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
-  let sampleCidrsLen = sampleCidrs.length;
-  // Create some test data for getIpv4MappedIpv6Address, both valid and invalid.
-  let sampleIpv4s = [ '172.16.10.1', '172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.256.1', '1.1.1.-1'];
-  let sampleIpv4sLen = sampleIpv4s.length;
-
-  // Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
-  for (let i = 0; i < sampleCidrsLen; i++) {
-    console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
-    // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
-    // The callback is using the fat arrow operator: () => { }
-    getFirstIpAddress(sampleCidrs[i], (data, error) => {
-      // Now we are inside the callback function.
-      // Display the results on the console.
-      if (error) {
-        console.error(`  Error returned from GET request: ${error}`);
-      }
-      console.log(`  Response returned from GET request: ${data}`);
-    });
-  }
-  // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
-  for (let i = 0; i < sampleIpv4sLen; i++) {
-    console.log(`\n--- Test Number ${i + 1} getIpv4MappedIpv6Address(${sampleIpv4s[i]}) ---`);
-    // Assign the function results to a variable so we can check if a string or null was returned.
-    let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
-    if( mappedAddress ) {
-      console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
-    } else {
-      console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
-    }
-  }
-}
-
-
 /**
  * Calculates an IPv4-mapped IPv6 address.
  * @param {string} ipv4 - An IPv4 address in dotted-quad format.
@@ -133,6 +93,46 @@ function getIpv4MappedIpv6Address(ipv4) {
     }
   }
   return ipv6Address;
+}
+
+
+/*
+  This section is used to test function and log any errors.
+  We will make several positive and negative tests.
+*/
+function main() {
+  // Create some test data for getFirstIpAddress(), both valid and invalid.
+  let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
+  let sampleCidrsLen = sampleCidrs.length;
+  // Create some test data for getIpv4MappedIpv6Address, both valid and invalid.
+  let sampleIpv4s = [ '172.16.10.1', '172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.256.1', '1.1.1.-1'];
+  let sampleIpv4sLen = sampleIpv4s.length;
+
+  // Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
+  for (let i = 0; i < sampleCidrsLen; i++) {
+    console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
+    // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
+    // The callback is using the fat arrow operator: () => { }
+    getFirstIpAddress(sampleCidrs[i], (data, error) => {
+      // Now we are inside the callback function.
+      // Display the results on the console.
+      if (error) {
+        console.error(`  Error returned from GET request: ${error}`);
+      }
+      console.log(`  Response returned from GET request: ${data}`);
+    });
+  }
+  // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
+  for (let i = 0; i < sampleIpv4sLen; i++) {
+    console.log(`\n--- Test Number ${i + 1} getIpv4MappedIpv6Address(${sampleIpv4s[i]}) ---`);
+    // Assign the function results to a variable so we can check if a string or null was returned.
+    let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
+    if( mappedAddress ) {
+      console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
+    } else {
+      console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
+    }
+  }
 }
 
 /*
